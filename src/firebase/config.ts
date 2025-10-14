2ceb53b2e0a85906bbb9b133002b8620f4788eee
It's AT JR <tiwariarpit3368@gmail.com>
2025-10-14 19:31:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5216412215-44f1a",
+  "appId": "1:649152294649:web:d8ba3ac9230c23e84bfc28",
+  "apiKey": "AIzaSyC_tpAShwuNDzdsUzMaoK_AXwbsJY4TW9w",
+  "authDomain": "studio-5216412215-44f1a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "649152294649"
+};
