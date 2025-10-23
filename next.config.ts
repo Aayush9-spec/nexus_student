17d4e68b336b4a3847246d9c72e66ecc734140ae
It's AT JR <tiwariarpit3368@gmail.com>
2025-10-23 17:46:51 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -32,8 +32,9 @@ const nextConfig: NextConfig = {
     ],
   },
   experimental: {
-    allowedDevOrigins: ["*.cloudworkstations.dev"],
+    
   },
+  allowedDevOrigins: ["*.cloudworkstations.dev"],
 };
 
 export default nextConfig;
