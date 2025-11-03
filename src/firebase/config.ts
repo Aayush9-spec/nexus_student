a5f666e5976f6f686ebe5d048dc54bdfd695ea60
It's AT JR <tiwariarpit3368@gmail.com>
2025-11-03 10:47:56 +0000

    where is the storage

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:649152294649:web:d8ba3ac9230c23e84bfc28",
   "apiKey": "AIzaSyC_tpAShwuNDzdsUzMaoK_AXwbsJY4TW9w",
   "authDomain": "studio-5216412215-44f1a.firebaseapp.com",
+  "storageBucket": "studio-5216412215-44f1a.appspot.com",
   "measurementId": "",
   "messagingSenderId": "649152294649"
 };
